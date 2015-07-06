9f010c9f8788491cadeb865238b4a60a9d924795
Nicholas Hydock <nhydock@gmail.com>
2015-07-05 22:56:21 -0400

    Wait until end of render cycle to swap screen to ensure stability

--- core/src/github/nhydock/boogie/Boogie.java
+++ core/src/github/nhydock/boogie/Boogie.java
@@ -3,10 +3,14 @@ package github.nhydock.boogie;
 import github.nhydock.boogie.view.MainScreen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
 public class Boogie extends Game {
     public static Game app;
     public static final String GAME_DIR = "bin/game/";
+    
+    Screen screen;
+    Screen queue;
     
 	@Override
 	public void create () {
@@ -14,6 +18,27 @@ public class Boogie extends Game {
         this.setScreen(screen);
         
         app = this;
+	}
+	
+	@Override
+	public void render() {
+	    super.render();
+	    
+	    if (this.queue != null) {
+	        super.setScreen(screen);
+	        this.screen = this.queue;
+	        this.queue = null;
+	    }
+	}
+	
+	@Override
+	public void setScreen(Screen screen) {
+	    if (this.screen == null) {
+	        super.setScreen(screen);
+	        this.screen = screen;
+	    } else {
+	        this.queue = screen;
+	    }
 	}
 
 }
